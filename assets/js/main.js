65665159d9e5495b4485a8ab0fa1f76154fbbb6a
Faruq Hossain <faruqdigital01@gmail.com>
2023-06-07 17:33:03 +0600

    js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,8 @@
-/**
-* Template Name: ComingSoon
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+
+(function () {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+  /*selector*/
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -20,9 +12,7 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
+  /*event listener*/
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -34,16 +24,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+  /*scroll event listener*/
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Back to top button
-   */
+  /*back to top button*/
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -57,13 +43,11 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Countdown timer
-   */
+  /*countdown timer*/
   let countdown = select('.countdown');
   const output = countdown.innerHTML;
 
-  const countDownDate = function() {
+  const countDownDate = function () {
     let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
